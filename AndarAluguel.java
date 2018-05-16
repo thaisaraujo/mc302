b8233049549217edbe77106235a8dafe0531a608
TIRI23 <atiradoparra@gmail.com>
2018-05-16 12:49:18 -0300

    Update AndarAluguel.java

--- AndarAluguel.java
+++ AndarAluguel.java
@@ -1,4 +1,5 @@
 
+
 public class AndarAluguel extends Andar { //inicio do metodo AndarAluguel
 	private final int MAX_CARRO = 10; //variavel magic number
 	
@@ -14,18 +15,21 @@ public class AndarAluguel extends Andar { //inicio do metodo AndarAluguel
 		this.limiteMultas = limiteMultas;
 		this.taxaMultas = taxaMultas; //chama construtor de super classe e atribui limite de multas e taxa
 				
-		carrosAluguel[0] = new CarroAluguel ("Marca 1", "Modelo1", "KHJ9531", "8372738", "vermelho", "80", 1999, "kgm101", "15000", 0);
-		carrosAluguel[1] = new CarroAluguel ("Marca 2", "Modelo2", "DME8366", "831234738", "verde", "44", 2006, "fjek1234", "10000", 1);
-		carrosAluguel[2] = new CarroAluguel ("Marca 1", "Modelo3", "LMD0293", "28381924", "vermelho", "70", 1989, "kek6942", "56000", 2);
-		carrosAluguel[3] = new CarroAluguel ("Marca 1", "Modelo1", "KEM3942", "312731271", "preto", "20", 1999, "kfdk323124", "10000", 3);
-		carrosAluguel[4] = new CarroAluguel ("Marca 2", "Modelo2", "MEM2938", "5838283289", "vermelho", "65", 2008, "mlemle9392", "2000", 4);
-		carrosAluguel[5] = new CarroAluguel ("Marca 3", "Modelo4", "LEL5464", "83828392", "branco", "70", 2003, "meme9492", "30000", 5);
-		carrosAluguel[6] = new CarroAluguel ("Marca 3", "Modelo4", "LEK3212", "75636234", "azul", "80", 2005, "ltoel2423", "22000", 6);
-		carrosAluguel[7] = new CarroAluguel ("Marca 1", "Modelo3", "MLK9531", "784744343", "vermelho", "40", 1997, "oeokd1234", "15000", 7);
-		carrosAluguel[8] = new CarroAluguel ("Marca 1", "Modelo1", "YHR8343", "4828841142", "vermelho", "50", 2009 , "bascw1234", "35000", 8);
-		carrosAluguel[9] = new CarroAluguel ("Marca 2", "Modelo2", "BAC2312", "1929293", "preto", "80", 2006, "lmwle21313", "5000", 9);
+		carrosAluguel[0] = new CarroAluguel ("Marca 1", "Modelo1", "KHJ9531", "8372738", "vermelho", "80", 2016, "kgm101", 15000, 0);
+		carrosAluguel[1] = new CarroAluguel ("Marca 2", "Modelo2", "DME8366", "831234738", "verde", "44", 2016, "fjek1234", 10000, 1);
+		carrosAluguel[2] = new CarroAluguel ("Marca 1", "Modelo3", "LMD0293", "28381924", "vermelho", "70", 2015, "kek6942", 51000, 2);
+		carrosAluguel[3] = new CarroAluguel ("Marca 1", "Modelo1", "KEM3942", "312731271", "preto", "20", 2014, "kfdk323124", 35000, 3);
+		carrosAluguel[4] = new CarroAluguel ("Marca 2", "Modelo2", "MEM2938", "5838283289", "vermelho", "65", 2017, "mlemle9392", 22000, 4);
+		carrosAluguel[5] = new CarroAluguel ("Marca 3", "Modelo4", "LEL5464", "83828392", "branco", "70", 2017, "meme9492", 20000, 5);
+		carrosAluguel[6] = new CarroAluguel ("Marca 3", "Modelo4", "LEK3212", "75636234", "azul", "80", 2018, "ltoel2423",2000, 6);
+		carrosAluguel[7] = new CarroAluguel ("Marca 1", "Modelo3", "MLK9531", "784744343", "vermelho", "40", 2017, "oeokd1234", 15000, 7);
+		carrosAluguel[8] = new CarroAluguel ("Marca 1", "Modelo1", "YHR8343", "4828841142", "vermelho", "50", 2016 , "bascw1234", 35000, 8);
+		carrosAluguel[9] = new CarroAluguel ("Marca 2", "Modelo2", "BAC2312", "1929293", "preto", "80", 2015, "lmwle21313", 50000, 9);
 		//cria objetos de carro aluguel em um vetor
 	} //fim do metodo construtor
+	
+	
+	
 	
 	public CarroAluguel buscaCarroDisponivel(ClienteAluguel cliente) { //inicio do metodo buscaCarroDisponivel
 		for(int i = 0; i < MAX_CARRO; i++) {
